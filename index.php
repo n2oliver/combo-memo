d0feef656153be90bcb32e137e11f8cef5e7ad32
Óliver Silva <silva.liver@gmail.com>
2025-06-20 20:41:23 -0300

    ajuste do logo

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
 <head>
   <meta charset="UTF-8">
   <title>Combo-Memo - Jogo da Memória</title>
+
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="icon" type="image/png" sizes="32x32" href="<?php $APP_URL ?>/components/card/coruja.png"/>
+
         <link rel="prefetch" href="<?= $APP_URL ?>/components/card/card.png" as="image" type="image/png" />
         <link rel="prefetch" href="<?= $APP_URL ?>/components/card/abelha.png" as="image" type="image/png" />
         <link rel="prefetch" href="<?= $APP_URL ?>/components/card/coruja.png" as="image" type="image/png" />
