afd64e224f605499fd54a363f2a305c226546c7f
Óliver Silva <silva.liver@gmail.com>
2025-08-12 19:56:36 -0300

    aads banner

--- index.php
+++ index.php
@@ -109,8 +109,9 @@
 
   <a href="/jogos/combo-memo/jogo.php" id="jogar-agora" class="btn btn-warning">Jogar Agora</a>
   </main>
-  <div class="text-light mx-auto mt-3 col-md-8 text-center">
-      Anúncios
+  <div id="frame" class="container m-auto col-md-8" style="width: 100%;margin: auto;background: rgba(0, 0, 0, 0.50);position: relative; z-index: 9;">
+      <iframe data-aa='2406506' src='//acceptable.a-ads.com/2406506/?size=Adaptive'
+          style='border:0; padding:0; width:70%; height:auto; overflow:hidden;display: block;margin: auto'></iframe>
   </div>
   <div class="container m-auto col-md-8 text-light">
       <script async="async" data-cfasync="false" src="//pl27079464.profitableratecpm.com/297c1c3c46604eb55408b6261c6dd5b3/invoke.js"></script>
