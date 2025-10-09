705fc104b7872e91958b81f4ef485268f1ff028d
n2oliver <silva.liver@gmail.com>
2025-10-09 19:44:53 -0300

    adiciona hotjar

--- jogo.php
+++ jogo.php
@@ -51,6 +51,17 @@ $APP_URL = "/jogos/combo-memo/";
                 color: pink;
             }
         </style>
+        <!-- Hotjar Tracking Code for n2oliver.com -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6543030,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
     </head>
     <body>
 <?php include("../../gtagmanager.php"); ?>
