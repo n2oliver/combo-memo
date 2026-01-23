7d112c8109f0fc7be790d7e98fb9c73f5db3c5c0
n2oliver <silva.liver@gmail.com>
2026-01-23 15:47:18 -0300

    melhora usabilidade de combo-memo

--- index.php
+++ index.php
@@ -137,25 +137,8 @@ if ($impressionid) {
 </head>
 <body>
   <script src="/js/anuncios.js"></script>
-  <script>
-    setTimeout(()=>{
-        abrirSmartlinkUmaVez();
-    }, 3200);
-  </script>
   <div style="width: 64px; height: 64px; background-image: url(../linhaamarela/icons8-alto-falante-100.png); background-size: contain; background-repeat: no-repeat; background-position: center"></div>
-  <audio preload="auto" hidden loop>
-    <source src="/jogos/combo-memo/components/song/combo-memo-theme.mp3" type="audio/mpeg">
-  </audio>
-  <script>
-    document.addEventListener('DOMContentLoaded', ()=>{
-      function tocarAudio() {
-        document.querySelector('audio').play();
-        document.removeEventListener('click', tocarAudio);
-      }
-      document.addEventListener('click', tocarAudio);
-    });
-  </script>
-    <script async src="https://appsha-pnd.ctengine.io/js/script.js?wkey=97NjKiTr7b"></script>
+  <script async src="https://appsha-pnd.ctengine.io/js/script.js?wkey=97NjKiTr7b"></script>
 
 <?php include("../../gtagmanager.php"); ?>
     <?php include('../../navbar.php'); ?>
@@ -212,12 +195,14 @@ if ($impressionid) {
           currency: "USD",
           value: 0.0004
         });
-        $('#jogar-agora').click(()=>{
-            window.location.href = '/jogos/combo-memo/jogo.php';
+        $('#jogar-agora').click(()=>{            
             gtag("event", "close_convert_lead", {
               currency: "USD",
               value: 0.0004
             });
+            setTimeout(()=>{
+              window.location.href = '/jogos/combo-memo/jogo.php';
+            }, 200);
         });
       });
     </script>
